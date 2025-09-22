f6e1ce0e1d77bc5e6931a30de2a316358cd62530
Ajmery Jaman <50037803+ajmeryjaman@users.noreply.github.com>
2025-09-22 12:19:02 -0400

    Update penalizedG.R

--- R/penalizedG.R
+++ R/penalizedG.R
@@ -47,10 +47,10 @@
 #' @examples
 #' library(mvtnorm)
 #' expit <- function(x) exp(x)/(1+exp(x))
-#'
+#' 
 #' ## data.gen is a function that generates a longitudinal data set for a specific correlation
 #' ## structure. Available structures in this function are: independence, exchangeable and ar1.
-#'
+#' 
 #' # Arguments(data.gen):
 #' #     n = Number of subjects
 #' #     ni = A vector containing number of time points for each subject
@@ -59,24 +59,24 @@
 #' #     corstr = The correlation structure among the repeated outcomes
 #' #     autocorr.coef = The autocorrelation coefficient for inducing correlation among the
 #' #     continuous confounders and the noise covariates
-#'
+#' 
 #' data.gen <- function(n, ni, sigma2.e, alpha, corstr, autocorr.coef){
 #'   # treatment model: a ~ 1+l1+...+l6 [logit link]
 #'   # treatment-free model:  ~ 1+l1+...+l6+exp(l5)
 #'   # blip model: a * (1+l1+...+l5)
 #'   # noise covariates: x1,...,x10
 #'   beta <- c(0, 1, -1.1, 1.2, 0.75, -0.9, 1.2) # treatment model parameters
-#'   delta <- c(1, 1, 1.2, 1.2, -0.9, 0.8, -1, 1) # treatment-free model parameters
-#'   psi <- c(1, 1, -1, -0.9, 0.8, 1, 0) # blip parameters
-#'
+#'   delta <- c(1, 1, 1.2, 1.2, -0.9, 0.8, -1, 1.3) # treatment-free model parameters
+#'   psi <- c(1, 1.5, -1, -1.2, 1.5, 1.2, 0) # blip parameters
+#' 
 #'   # generating two continuous baseline covariates
 #'   l1 <- rnorm(n, 0, 1)
 #'   l2 <- rnorm(n, 0, 1)
-#'
+#' 
 #'   # V is the covariance matrix of the time-varying confounders (l3,..,l6) and
 #'   # noise covariates (x1,...,x10)
 #'   V <- toeplitz(autocorr.coef^(0:(14-1)))
-#'
+#' 
 #'   lx <- a <- y <- vector(mode="list", length=n)
 #'   lx.mat <- NULL
 #'   for(i in 1:n){
@@ -84,7 +84,7 @@
 #'     lx[[i]] <- matrix(NA, ni[i], 16)
 #'     lx[[i]][,1] <- rep(l1[i], ni[i])
 #'     lx[[i]][,2] <- rep(l2[i], ni[i])
-#'
+#' 
 #'     corr.mat <- switch(corstr,
 #'                        "exchangeable" = toeplitz(c(1, rep(alpha, ni[i]-1))),
 #'                        "ar1" = toeplitz(alpha^(0:(ni[i]-1))),
@@ -92,8 +92,8 @@
 #'     )
 #'     cov.mat <- diag(sqrt(sigma2.e), ni[i]) %*% corr.mat %*% diag(sqrt(sigma2.e), ni[i])
 #'     e <- rmvnorm(1, sigma = cov.mat)
-#'
-#'     ### j=1
+#' 
+#'     ## j=1
 #'     mu.l <- rep(0, 4) #rep(0.3*lx[[i]][1, 1]+0.3*lx[[i]][1, 2],4)
 #'     mu.x <- rep(0,10)
 #'     lx[[i]][1,3:16] <- mvtnorm::rmvnorm(1, mean=c(mu.l,mu.x), sigma = V)
@@ -101,7 +101,7 @@
 #'     tf.mean <- sum(c(1, lx[[i]][1,1:6], exp(lx[[i]][1,5])) * delta)
 #'     blip <- sum(c(a[[i]][1], a[[i]][1]*c(lx[[i]][1,1:6])) * psi)
 #'     y[[i]][1] <- (tf.mean + blip) + e[1]
-#'
+#' 
 #'     ## j=2:ni
 #'     for(j in 2:ni[i]){
 #'       mu.l <- 0.3*lx[[i]][j-1, 3:6] + 0.3*a[[i]][j-1]
@@ -114,7 +114,7 @@
 #'     }
 #'     lx.mat <- rbind(lx.mat, lx[[i]])
 #'   }
-#'
+#' 
 #'   colnames(lx.mat) <- c(paste("l", 1:6, sep=""), paste("x", 1:10, sep=""))
 #'   data <- data.frame(id=rep(1:n, times=ni), a=unlist(a), lx.mat, y=round(unlist(y),3))
 #'   return(data)
@@ -122,36 +122,46 @@
 #' # Generate the data
 #' data.s <- data.gen(n = 500, ni = rep(6, 500), sigma2.e = 1, alpha = 0.8,
 #'                    corstr = "exchangeable", autocorr.coef = 0)
-#'
+#' 
 #' # Treatment-free model is misspecified, because we did not include exp(l5) in it
 #' tf.model <- as.formula(paste("~",paste(c(paste("l",1:6,sep=""), paste("x",1:10,sep="")),
 #'                                                    collapse = "+"), collapse=""))
-#' # Treatment model is correctly specified                                                 
-#' treat.model <- ~l1+l2+l3+l4+l5+l6
+#' 
+#' treat.model <- ~l1+l2+l3+l4+l5+l6 # Treatment model is correctly specified
+#' # treat.model <- ~1 # Use this to see what happens if the treatment model is misspecified
+#' 
 #' p <- length(model.matrix(tf.model, data=data.s)[1,])
+#' 
+#' ## penG() performs penalized G-estimation for a single value of tuning parameter lambda
+#' ## We can see the estimates from the full model by setting lambda = 0.01 (a small value)
+#' out <- penG(data = data.s, wc.str = "exchangeable", id.var="id", response.var="y",
+#'             treat.var="a", tf.model=tf.model, treat.model = treat.model,
+#'             lambda = 0.01, maxitr = 50, penalty = "SCAD")
+#' out$nitr
+#' round(out$estimate[1:7], 2)
 #' 
 #' # Creating a sequence of tuning parameter values in decreasing order
 #' lam_max <- 1
 #' lam_min <- 0.01*lam_max
 #' lambda.seq <- sort(seq(lam_min,lam_max,(lam_max-lam_min)/99), decreasing=TRUE)
-#'
+#' 
 #' # Perfom the penalized G-estimation for all lambda values and get the fit corresponding
 #' # to the optimal lambda chosen by a Doubly Robust Information Criterion (DRIC)
 #' out <- penalizedG(data = data.s, wc.str = "exchangeable", id.var="id", response.var="y",
 #'                    treat.var="a", tf.model=tf.model, treat.model = treat.model,
 #'                    lambda.seq = lambda.seq, maxitr = 50, penalty = "SCAD")
-#'
+#' 
 #' names(out)
 #' out$lambda.optimal
 #' out$nitr
 #' out$selected.EMs
-#'
+#' 
 #' effect.modification <- out$estimate[2:p]
 #' names(effect.modification) <- row.names(out$estimate)[2:p]
 #' effect.modification <- effect.modification[abs(effect.modification) > 0.001]
-#'
+#' 
 #' main.effect <- out$estimate[1]
-#'
+#' 
 #' round(main.effect, 2)
 #' round(effect.modification, 2)
 #' 
